4bb43e0d3705564252f123ec76a5da914cef6a62
macFyfe <macspersonalemail@gmail.com>
2019-11-17 16:51:04 -0600

    HOTFIX- filled out more of form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,7 +9,7 @@ class CharactersController < ApplicationController
 
     def create
         
-        character = Character.new(params.require(:character).permit(:background, :character_class, :race, :user_id ))
+        character = Character.new(params.require(:character).permit(:background, :character_class, :race, :user_id, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma ))
         character.user_id = current_user.id
         puts "CURRENT USER ID"
         puts current_user.id
